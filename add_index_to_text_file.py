b814e43b3cd16d5d95ff6126b49e09ea7faf2ea8
anahita.safari <anahita.safari@koresoftware.com>
2022-04-19 16:47:53 -0700

    first line explanetory

--- add_index_to_text_file.py
+++ add_index_to_text_file.py
@@ -1,3 +1,5 @@
+#### AppData\Roaming\JetBrains\PyCharmCE2020.3\scratches>
+
 with open('C:/Users/anahita.safari\Downloads\883\FB_Exports_2022-03-24T2213_cz4h5R_v4\dashboard-f&b_exports/order_state_transitions.csv') as infile, open('C:/Users/anahita.safari\Downloads\883\FB_Exports_2022-03-24T2213_cz4h5R_v4\dashboard-f&b_exports/order_state_transitions_.csv', 'w') as outfile:
     for idx, line in enumerate(infile):
         outfile.write(f'{idx},{line}')

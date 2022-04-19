0582139a74a0881979da008180b80f6db9ea3e4a
anahita.safari <anahita.safari@koresoftware.com>
2022-04-19 16:37:13 -0700

    this files adds index to each row of the text file

---
+++ add_index_to_text_file.py
@@ -0,0 +1,3 @@
+with open('C:/Users/anahita.safari\Downloads\883\FB_Exports_2022-03-24T2213_cz4h5R_v4\dashboard-f&b_exports/order_state_transitions.csv') as infile, open('C:/Users/anahita.safari\Downloads\883\FB_Exports_2022-03-24T2213_cz4h5R_v4\dashboard-f&b_exports/order_state_transitions_.csv', 'w') as outfile:
+    for idx, line in enumerate(infile):
+        outfile.write(f'{idx},{line}')
